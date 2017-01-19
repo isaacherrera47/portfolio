e3273f2439a30b9e8c8927bde62e009d95f0d764
Isaac Herrera <isaacherrera47@gmail.com>
2017-01-18 18:21:56 -0800

    + Added lenguaje on index page

--- resources/views/proyectos/index.blade.php
+++ resources/views/proyectos/index.blade.php
@@ -4,12 +4,12 @@
         <div class="row">
             @foreach($proyectos as $proyecto)
                 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 masonry-item">
-                    <div class="box-masonry"><a href="{{url('proyectos/'. $proyecto->id)}}" title=""
+                    <div class="box-masonry"><a href="{{url('proyectos/'. $proyecto->id)}}"
                                                 class="box-masonry-image with-hover-overlay with-hover-icon"><img
                                     src="{{asset('img/covers/'.$proyecto->portada)}}"
                                     class="img-responsive"></a>
                         <div class="box-masonry-text">
-                            <h4><a href="{{url('proyectos/'.$proyecto->id)}}">{{$proyecto->nombre}}</a></h4>
+                            <h4><a href="{{url('proyectos/'.$proyecto->id)}}">{{$proyecto->nombre}}</a> <small>{{$proyecto->lenguaje}}</small></h4>
                             <div class="box-masonry-desription">
                                 <p>{{$proyecto->extracto}}</p>
                             </div>
